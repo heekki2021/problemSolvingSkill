a2ac1efd20a32a730b0f27799f1ebe339e0c3bc6
BSJ <kfri1999@gmail.com>
2023-03-29 01:42:19 +0900

    03-28

--- week3/roman_to_cardinal_v2.cpp
+++ week3/roman_to_cardinal_v2.cpp
@@ -1,27 +1,3 @@
 #include <iostream>
-#include <string>
-#include <unordered_map>
-
-int romanToInt(const std::string &roman){
-	std::unordered_map<char, int> romanValues = {
-		{'l', 1},
-		{'V', 5},
-		{'X', 10},
-		{'L', 50},
-		{'C', 100},
-		{'D', 500},
-		{'M', 1000}
-	};
-
-	int result = 0;
-	int preValue = 0;
-
-	for (int i = roman.size() - 1; i >= 0; --i) {
-		char currentChar = roman[i];
-		int currentValue = romanValues[currentChar];
-
-		if (currentValue < preValue) {
-			result -= currentValue;
-		}
-	}
-}
+#include <vector>
+#include <string>dddfasdfdsaf
