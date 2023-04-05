db1e82131e1ecbfba9b46fd308918aede1e73307
BSJ <kfri1999@gmail.com>
2023-04-06 01:37:55 +0900

    4주차 과제 완료 (자식 노드가 4개인 트리)

--- week4/Binary Tree.cpp
+++ week4/Binary Tree.cpp
@@ -5,7 +5,44 @@ using namespace std;
 class Node
 {
 public:
-	Node *child[4];
+	int data;
+	Node** child;
 
-	// TreeNode(int value) : value(value), child[]
-}
+	Node() {
+		child = new Node * [4];
+		for (int i = 0; i < 4; ++i) {
+			child[i] = nullptr;
+		}
+	}
+
+	~Node() {
+		delete[] child;
+	}
+};
+
+class Tree {
+private:
+	Node* root;
+
+public:
+	// 게터 함수
+	Node* getRoot() const {
+		return root;
+	}
+
+	// 세터 함수
+	void setRoot(Node* newRoot) {
+		root = newRoot;
+	}
+
+	Node* makeNode(Node* right, Node* left, Node* up, Node* down, int data) {
+		Node* node = new Node();
+		node->data = data;
+		node->child[0] = right;
+		node->child[1] = left;
+		node->child[2] = up;
+		node->child[3] = down;
+		return node;
+	}
+
+};
