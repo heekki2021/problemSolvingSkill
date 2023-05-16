1b7a2996ac5f0491f55a46cfc96c5869bed41bb9
BSJ <kfri1999@gmail.com>
2023-05-16 12:22:28 +0900

    testing

---
+++ week10/database.c
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main() {
+
+	std::locale::global(std::locale("ko_KR.utf8"));
+
+	std::ifstream file1("input.txt");
+
+	if (!file1) {
+		std::cout << "파일을 열 수 없습니다." << std::endl;
+		return 1;
+	}
+
+	std::string formatStr, dataStr;
+
+	//두 줄 읽기
+	std::getline(file1, formatStr);
+	std::getline(file1, dataStr);
+
+	file1.close();
+
+	std::cout << "Format string: " << formatStr << std::endl;
+	std::cout << "Data string: " << dataStr << std::endl;
+
+
+
+}
